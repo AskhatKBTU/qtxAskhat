ccd4cb2ce6c0a26ee298fcfef9aec8f2fcb23e9d
AskhatKBTU <askhat.good@gmail.com>
2020-02-29 08:01:18 -0500

    Checking Commit with eGit first time

---
+++ src/test/java/NewTest.java
@@ -0,0 +1,17 @@
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+  @Test
+  public void myFirstTest() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
