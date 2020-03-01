6e631230ab2e64967bde84149e3c5e470d803d14
AskhatKBTU <askhat.good@gmail.com>
2020-02-29 19:16:12 -0500

    New Testing completed

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -1,18 +1,48 @@
 import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 
+
 public class NewTest {
+	
+	public WebDriver driver;
+	
+  @BeforeTest
+	  public void beforeTest() {
+		  
+		  WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+			String url = "http://google.com";
+			driver.navigate().to(url);
+	  }
+  
+  
+@AfterTest
+
+	  public void afterTest() {
+			driver.quit();
+		  
+	  }	
   @Test
   public void myFirstTest() {
-	System.out.println("New test");
+	System.out.println("Hello World");
 	}
-  @BeforeTest
-  public void beforeTest() {
-  }
+  
 
-  @AfterTest
-  public void afterTest() {
+  
+  @Test
+  public void launchChromeDriver() {
+	  
+	  	
+		
+		Assert.assertEquals(driver.getTitle(),"Google", "an Incorrect match");
+		
   }
 
 }
